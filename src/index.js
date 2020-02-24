3fef377b174ca20e87988519b9c8295a66c3788d
nurik91boss@gmail.com <nurik91boss@gmail.com>
2020-02-24 23:52:37 +0600

    feat: add reverse function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    return parseInt(
+        n.toString()
+          .split('')
+          .reverse()
+          .join('')
+      )
+                 
 }
